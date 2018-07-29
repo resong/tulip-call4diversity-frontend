0fc9afc3707f399cce2bd49bf7288eb268be1e72
M.K. Reyes <mkgreyes@gmail.com>
2018-07-29 09:03:36 -0400

    #5: Form UI (doesn't include hooking up state and validation) (#11)
    * markup for SubmissionForm component
    
    * trailing line at EOF

---
+++ src/components/Form/SubmissionForm.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class SubmissionForm extends Component {
+  render () {
+    return (
+      <form action="">
+        {/* Input fields here */}
+      </form>
+    );
+  }
+}
+
+export default SubmissionForm;
