2b650f9f5f04868a90bf430941578735f0f5eecd
M.K. Reyes <mkgreyes@gmail.com>
2018-08-16 10:58:08 -0400

    Merging in - the case of the missing styles (#41)

--- src/components/Form/DatePicker.js
+++ src/components/Form/DatePicker.js
@@ -4,7 +4,7 @@ import DatePicker from "react-widgets/lib/DatePicker";
 const WrappedDatePicker = ({label, ...props}) => {
   const onChange = props.input.onChange;
 
-  return <div>{label}<DatePicker onChange={onChange}/></div>;
+  return <div><span>{label}</span><DatePicker onChange={onChange}/></div>;
 };
 
 export default WrappedDatePicker;
